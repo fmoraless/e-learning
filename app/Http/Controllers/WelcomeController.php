b8188f1314ff5c5c96816bb298c35e1b354dcd93
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-03-23 02:18:27 -0300

    Mostrar cursos destacados

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index(){
         $categories = Category::withCount("courses")->get();
-        return view('welcome', compact('categories'));
+        $featuredCourses = Course::withCount("students")
+            ->with("categories", "teacher")
+            ->whereFeatured(true)
+            ->whereStatus(Course::PUBLISHED)
+            ->get();
+        $total = $featuredCourses->count();
+        //dd($total);
+
+        return view('welcome', compact('categories', 'featuredCourses'));
 
     }
 }
--- resources/views/partials/learning/featured_courses.blade.php
+++ resources/views/partials/learning/featured_courses.blade.php
@@ -2,171 +2,50 @@
 <section class="course-section spad">
     <div class="container">
         <div class="section-title mb-0">
-            <h2>Featured Courses</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
+            <h2>{{ __("Cursos destacados") }}</h2>
+            <p>{{ __("Aquí tienes los cursos destacados de la plataforma.") }}</p>
         </div>
     </div>
     <div class="course-warp">
         <ul class="course-filter controls">
-            <li class="control active" data-filter="all">All</li>
-            <li class="control" data-filter=".finance">Finance</li>
-            <li class="control" data-filter=".design">Design</li>
-            <li class="control" data-filter=".web">Web Development</li>
-            <li class="control" data-filter=".photo">Photography</li>
+            <li class="control active" data-filter="all">{{ __("Todos") }}</li>
+            @forelse($categories as $category)
+                <li class="control" data-filter=".{{ $category->name }}">
+                    {{ $category->name }}
+                </li>
+            @empty
+                <div class="empty-results">
+                    {!! __("No hay categorías disponibles") !!}
+                </div>
+            @endforelse
         </ul>
         <div class="row course-items-area">
+            @forelse($featuredCourses as $course)
             <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 finance">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/1.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>Art & Crafts</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
+                <div class="mix col-lg-3 col-md-4 col-sm-6 @foreach($course->categories as $category) {{ $category->name }} @endforeach">
+                    <div class="course-item">
+                        <div class="course-thumb set-bg" data-setbg="{{ $course->imagePath() }}">
+                            <div class="price">{{ __("Precio:price $", ["price" => $course->price]) }}</div>
                         </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/1.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
+                        <div class="course-info">
+                            <div class="course-text">
+                                <h5>{{ $course->title }}</h5>
+                                <div class="students">{{ __(":count Estudiantes", ['count' => $course->students_count]) }}</div>
+                            </div>
+                            <div class="course-author">
+                                <div class="ca-pic set-bg" data-setbg="/img/authors/1.jpg"></div>
+                                <p>{{ $course->teacher->name }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 design">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/2.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>IT Development</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/2.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
+            @empty
+                <div class="empty-results">
+                    {!! __("No hay cursos destacados disponibles") !!}
                 </div>
-            </div>
+            @endforelse
             <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 web">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/3.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>Graphic Design</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/3.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 photo">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/4.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>IT Development</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/4.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 finance">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/5.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>IT Development</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/5.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 design">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/6.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>Socia Media</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/6.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 web">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/7.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>IT Development</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/7.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- course -->
-            <div class="mix col-lg-3 col-md-4 col-sm-6 photo">
-                <div class="course-item">
-                    <div class="course-thumb set-bg" data-setbg="img/courses/8.jpg">
-                        <div class="price">Price: $15</div>
-                    </div>
-                    <div class="course-info">
-                        <div class="course-text">
-                            <h5>HTML 5</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur</p>
-                            <div class="students">120 Students</div>
-                        </div>
-                        <div class="course-author">
-                            <div class="ca-pic set-bg" data-setbg="img/authors/8.jpg"></div>
-                            <p>William Parker, <span>Developer</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
     </div>
 </section>
