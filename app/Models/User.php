2a3f56963fcdd2c7dd5b89b5bc913bf3e8312535
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-05 00:08:15 -0400

    Instalacion laravel Cashier
     - composer require laravel/cashier
    - php artisan vendor:publish --tag="cashier-migrations"

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 /**
  * App\Models\User
@@ -38,7 +39,7 @@ use Illuminate\Notifications\Notifiable;
  */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Billable;
 
     const ADMIN = 'ADMIN';
     const TEACHER = 'TEACHER';
