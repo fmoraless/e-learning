796ef07ef684313835dc64cd9ec1a8395afb8e91
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-07 00:05:15 -0400

    Metodo para capturar evento ChargeSucceeded

--- app/Http/Controllers/StripeWebHookController.php
+++ app/Http/Controllers/StripeWebHookController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Response;
 use Laravel\Cashier\Http\Controllers\WebhookController;
+use PharIo\Version\Exception;
+use Log;
 
 class StripeWebHookController extends WebhookController
 {
@@ -17,6 +19,37 @@ class StripeWebHookController extends WebhookController
      */
 
     public function handleChargeSucceeded($payload) {
-        return new Response('Webhook Handled: {handleChargeSucceeded}', 200);
+        try {
+            $invoice_id = $payload['data']['object']["invoice"];
+            $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+            if ($user) {
+                $order = $user->orders()
+                        ->where("status", Order::PENDING)
+                        ->latest()
+                        ->first();
+                if ($order) {
+                    $order->update([
+                       'invoice_id' => $invoice_id,
+                       'status'     => Order::SUCCESS
+                    ]);
+
+                    //ATTACH COURSES FOR USER
+                    $coursesId = $order->orderLines()->pluck("course_id");
+                    Log::info(json_encode($coursesId));
+                    $user->courses_learning()->attach($coursesId);
+
+
+                    Log::info(json_encode($user));
+                    Log::info(json_encode($order));
+                    Log::info("Pedido actualizado correctamente");
+
+                    return new Response('Webhook Handled: {handleChargeSucceeded}',200);
+                }
+            }
+        } catch (\Exception $exception){
+            Log::debug("Excepción Webhook {handleChargeSucceeded}: " . $exception->getMessage() . ", Line: " . $exception->getLine() . ', File: ' . $exception->getFile());
+            return new Response('Webhook Handled: {handleChargeSucceeded}', 400);
+        }
+
     }
 }
