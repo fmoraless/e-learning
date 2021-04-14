48803b2bc8bd66abdd29694368feb5b3f1ce443f
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-14 02:10:13 -0400

    Navegacion por el curso
    Botonera inferior y Sidebar Widget

--- resources/views/partials/learning/courses/learn/sidebar.blade.php
+++ resources/views/partials/learning/courses/learn/sidebar.blade.php
@@ -8,25 +8,24 @@
             @forelse($course->units as $unit)
                 @switch($unit->unit_type)
                     @case(\App\Models\Unit::SECTION)
-                        <div class="bg-dark text-white p-0 text-center">
-                            {{ $unit->title }}
-                        </div>
+                    <div class="bg-dark text-white p-0 text-center">
+                        {{ $unit->title }}
+                    </div>
                     @break
                     @case(\App\Models\Unit::ZIP)
-                        <div class="card-text p-2 border-bottom unit">
-                            <a
-                                id="unit-{{ $index }}"
-                                href="#"
-                                class="text-black-50"
-                                data-type="{{ \App\Models\Unit::ZIP }}"
-                                data-unit="{{ $unit }}"
-                                data-index="{{ $index }}"
-                            >
-                                <i class="fa fa-file-zip-o"></i> {{ $unit->title }}
-                            </a>
-                        </div>
+                    <div class="card-text p-2 border-bottom unit">
+                        <a
+                            id="unit-{{ $index }}"
+                            href="#"
+                            class="text-black-50"
+                            data-type="{{ \App\Models\Unit::ZIP }}"
+                            data-unit="{{ $unit }}"
+                            data-index="{{ $index }}"
+                        >
+                            <i class="fa fa-file-zip-o"></i> {{ $unit->title }}
+                        </a>
+                    </div>
                     @break
-
                     @case(\App\Models\Unit::VIDEO)
                     <div class="card-text p-2 border-bottom unit">
                         <a
@@ -42,7 +41,7 @@
                     </div>
                     @break
                 @endswitch
-                @if($unit->unit_type !== \App\Models\Unit::SECTION )
+                @if($unit->unit_type !== \App\Models\Unit::SECTION)
                     @php $index++ @endphp
                 @endif
             @empty
@@ -51,6 +50,16 @@
                 </div>
             @endforelse
         </div>
+        @can('review', $course)
+            <div class="card-footer">
+                <a
+                    href="{{ route('courses.reviews.create', ['course' => $course]) }}"
+                    class="site-btn btn-block"
+                >
+                    {{ __("Valorar el curso") }}
+                </a>
+            </div>
+        @endcan
     </div>
 </div>
 
@@ -59,16 +68,71 @@
         let index = null;
         jQuery(document).ready(function () {
             jQuery(".unit").on("click", function (e) {
-               e.preventDefault();
-               const link = $(this).find("a");
-               /*const type = link.data("type");
-               const unit = link.data("unit");
-               console.log(type);
-               console.log(unit);*/
+                e.preventDefault();
+                const link = $(this).find("a");
                 $(".unit").removeClass("unit-selected");
                 $(this).addClass("unit-selected");
                 index = link.data("index");
+                const unit = link.data("unit");
+                const type = link.data("type");
+                const visorHeader = $("#visor-card .card-header");
+                const visorBody = $("#visor-card .card-body");
+                const visorFooter = $("#visor-card .card-footer");
+                const downloadText = '{{ __("Descargar") }}';
+                const prevUnitText = '{{ __("Anterior unidad") }}';
+                const nextUnitText = '{{ __("Siguiente unidad") }}';
+                visorHeader.text(unit.title);
+                if (type === '{{ \App\Models\Unit::ZIP }}') {
+                    const url = `/storage/units/${unit.file}`;
+                    const html = `
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td width="90%">${unit.file}</td>
+                                <td><a href="#" onclick="window.location.href = '${url}'">${downloadText}</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                `;
+                    visorBody.html(html);
+                }
+                if (type === '{{ \App\Models\Unit::VIDEO }}') {
+                    const html = `
+                    <div class="embed-responsive embed-responsive-16by9">
+                        ${unit.content}
+                    </div>
+               `;
+                    visorBody.html(html);
+                }
+
+                let footerButtons = '';
+                if ($(`#unit-${index+1}`).length) {
+                    footerButtons += `
+                  <button class="site-btn float-right loadNextUnit">
+				  ${nextUnitText}</button>
+               `;
+                }
+                if ($(`#unit-${index-1}`).length) {
+                    footerButtons += `
+                  <button class="site-btn float-left loadPrevUnit">
+				  ${prevUnitText}</button>
+               `;
+                }
+                visorFooter.html(footerButtons);
             });
-        });
+
+            const visor = $("#visor-card");
+            visor.on("click", ".loadNextUnit", function () {
+                const nextIndex = index += 1;
+                $(`#unit-${nextIndex}`).click();
+            });
+            visor.on("click", ".loadPrevUnit", function () {
+                const prevIndex = index -= 1;
+                $(`#unit-${prevIndex}`).click();
+            });
+        })
     </script>
 @endpush
+
+
+
