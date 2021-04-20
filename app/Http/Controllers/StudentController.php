533cd192b6b6ff8c2050ef21e2830d12b961b843
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-20 02:41:16 -0400

    import wishlist Trait

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Traits\ManageCart;
 use App\Traits\Student\ManageCourses;
 use App\Traits\Student\ManageOrders;
+use App\Traits\Student\ManageWishLists;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    use ManageCart, ManageCourses, ManageOrders;
+    use ManageCart, ManageCourses, ManageOrders, ManageWishLists;
 
     public function index() {
         return view('student.index');
