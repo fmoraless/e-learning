e11b4307e6ef43be50b587f9e1f6bebfcb90261c
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-20 02:46:21 -0400

    Pass wishlist relation on welcome view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,7 +11,7 @@ class WelcomeController extends Controller
     public function index(){
         $categories = Category::withCount("courses")->get();
         $featuredCourses = Course::withCount("students")
-            ->with("categories", "teacher")
+            ->with("categories", "teacher", "wishlists")
             ->whereFeatured(true)
             ->whereStatus(Course::PUBLISHED)
             ->get();
